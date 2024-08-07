10018b84a299bf4766e4b3cbf0e0d859fa74e237
winternotseason <winternotseason@naver.com>
2024-08-07 17:16:54 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const axios = require("axios");
+const cors = require("cors");
+const app = express();
+const port = 8080;
+
+
+app.listen(port, () => console.log("8080 server"));
