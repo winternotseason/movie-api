a5a444524f1b05b983796c964c4529c50a4f7b28
winternotseason <winternotseason@naver.com>
2024-08-07 18:02:53 +0900

    response 처리

--- app.js
+++ app.js
@@ -3,35 +3,29 @@ const axios = require("axios");
 const cors = require("cors");
 const app = express();
 
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
-app.use(cors({
-    origin: '*'
-}));
+const getData = async (releaseDts, movieNm) => {
+  let res;
+  try {
+    res = await axios.get(
+      `https://api.koreafilm.or.kr/openapi-data2/wisenut/search_api/search_json2.jsp?collection=kmdb_new2&ServiceKey=${process.env.KMDB_API_KEY}&releaseDts=${releaseDts}&query=${movieNm}`
+    );
+    return response;
+  } catch (err) {
+    console.error(err);
+  }
+};
 
-
-const getData = async (releaseDts,movieNm) => {
-    let res;
-    try {
-        res = await axios.get(`https://api.koreafilm.or.kr/openapi-data2/wisenut/search_api/search_json2.jsp?collection=kmdb_new2&ServiceKey=${
-        process.env.KMDB_API_KEY
-      }&releaseDts=${releaseDts}&query=${movieNm}`);
-      return response
-    } catch (err) {
-        console.error(err)
-    }
-}
-
-
-app.get('/api' , async (req, res) => {
-    const {releaseDts, query: movieNm} = req.query
-
-    try {
-        const data = await getData(releaseDts, movieNm);
-        res.json(data)
-    } catch (err) {
-        res.status(500).send('KMDB 연결 에러')
-    }
-})
-
+app.get("/api", (req, res) => {
+  const { releaseDts, query: movieNm } = req.query;
+  getData(releaseDts, movieNm).then((response) => {
+    res.json(response.data)
+  })
+});
 
 module.exports = app;
