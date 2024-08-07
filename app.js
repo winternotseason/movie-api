c50a72ae5d96d975d6106a990c53978588048dde
winternotseason <winternotseason@naver.com>
2024-08-07 19:17:38 +0900

    /api/detail /api/serach

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.use(
   })
 );
 
-const getData = async (releaseDts, movieNm) => {
+const getChartData = async (releaseDts, movieNm) => {
   try {
     const response = await axios.get(
       `https://api.koreafilm.or.kr/openapi-data2/wisenut/search_api/search_json2.jsp?collection=kmdb_new2&ServiceKey=${process.env.KMDB_API_KEY}&releaseDts=${releaseDts}&query=${movieNm}`
@@ -20,12 +20,47 @@ const getData = async (releaseDts, movieNm) => {
   }
 };
 
+const getDetailInfo = async (releaseDts, title) => {
+  try {
+    const response = await axios.get(
+      `//api.koreafilm.or.kr/openapi-data2/wisenut/search_api/search_json2.jsp?collection=kmdb_new2&ServiceKey=${process.env.KMDB_API_KEY}&releaseDts=${releaseDt}&title=${title}`
+    );
+    return response;
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+const getSearchData = async (title) => {
+  try {
+    const response = await axios.get(
+      `//api.koreafilm.or.kr/openapi-data2/wisenut/search_api/search_json2.jsp?collection=kmdb_new2&ServiceKey=${process.env.KMDB_API_KEY}&title=${title}`
+    );
+    return response;
+  } catch (err) {
+    console.error(err);
+  }
+};
+
 app.get("/api", (req, res) => {
-  const { releaseDts, query } = req.query;
-  const movieNm = decodeURIComponent(query)
-  getData(releaseDts, movieNm).then((response) => {
+  const { releaseDts, query: movieNm } = req.query;
+  getChartData(releaseDts, query).then((response) => {
+    res.json(response.data);
+  });
+});
+
+app.get("/api/detail", (req, res) => {
+  const { releaseDts, title } = req.query;
+  getDetailInfo(releaseDts, title).then((response) => {
+    res.json(response.data);
+  });
+});
+
+app.get('/api/search', (req,res) => {
+  const { title } = req.query;
+  getSearchData(title).then((response) => {
     res.json(response.data)
   })
-});
+})
 
 module.exports = app;
